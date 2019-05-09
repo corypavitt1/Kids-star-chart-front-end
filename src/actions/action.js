1df95c93e3e81b0787db984bceb21fa1d6849c09
Cory Pavitt <corypavitt@hotmail.com>
2019-05-09 12:44:17 -0400

    update after sorting and rvm and ruby update

--- src/actions/action.js
+++ src/actions/action.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://kids-reward-star-chart.herokuapp.com"
-const BASE_URL = "http://localhost:3000/"
+ const BASE_URL = "https://kids-reward-star-chart.herokuapp.com"
+//const BASE_URL = "http://localhost:3000/"
 //----------choreslected true of false------
 
 export function choresSelected() {
